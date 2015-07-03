53f9fa0c214c31c99f53a247cd0bd04a62ee196a
Peter Westmacott <peter.westmacott@gmail.com>
2015-07-03 22:07:14 +0100

    Created shading layer to give 3D effect to dots.

--- src/dorothy/core.clj
+++ src/dorothy/core.clj
@@ -8,24 +8,57 @@
   (when (SystemTray/isSupported) (SystemTray/getSystemTray)))
 
 (def ^:private icon-size
-  "Width, height and inset for drawing icons."
+  "Size and inset for drawing icons."
   (let [dimension (.getTrayIconSize system-tray)
-                               width (int (.getWidth dimension))
-                               height (int (.getHeight dimension))
-                               inset 3]
-                           [width height inset]))
+        width (int (.getWidth dimension))
+        height (int (.getHeight dimension))
+        size (min width height)
+        inset 2]
+    [size inset]))
+
+(defn- distance
+  "Calculates the distance between two points."
+  [x1 y1 x2 y2]
+  (let [dx (- x1 x2)
+        dy (- y1 y2)]
+    (Math/sqrt (+ (* dx dx) (* dy dy)))))
+
+(def ^:private shading
+  "A shaded image to be overlaid onto a dot giving the appearance of sphericality."
+  (let [[size inset] icon-size
+        image (BufferedImage. size size BufferedImage/TYPE_INT_ARGB)
+        graphics (.getGraphics image)
+        half (/ size 2)
+        third (/ size 3)
+        inner-half (- half inset)]
+    (.setRenderingHint graphics RenderingHints/KEY_ANTIALIASING RenderingHints/VALUE_ANTIALIAS_ON)
+    (doseq [x (range size)
+            y (range size)]
+      (let [to-centre (distance half half x y)]
+        (when (< to-centre inner-half)
+          (let [to-highlight (distance third third x y)]
+            (cond
+              ; Highlight
+              (< to-highlight inner-half) (let [alpha (int (* 200 (- 1 (/ to-highlight inner-half))))]
+                                            (.setRGB image x y (.getRGB (Color. 255 255 255 alpha))))
+              ; Shade
+              (> to-highlight inner-half) (let [alpha (int (* 200 (/ (- to-highlight inner-half) third)))]
+                                            (.setRGB image x y (.getRGB (Color. 0 0 0 alpha)))))))))
+    ; Border
+    (.setColor graphics Color/BLACK)
+    (.drawArc graphics inset inset (- size inset inset) (- size inset inset) 0 360)
+    image))
 
 (defn- make-image
   "Create an image that can be used in a dot."
   [colour]
-  (let [[width height inset] icon-size
-        image (BufferedImage. width height BufferedImage/TYPE_INT_ARGB)
+  (let [[size inset] icon-size
+        image (BufferedImage. size size BufferedImage/TYPE_INT_ARGB)
         graphics (.getGraphics image)]
     (.setRenderingHint graphics RenderingHints/KEY_ANTIALIASING RenderingHints/VALUE_ANTIALIAS_ON)
     (.setColor graphics colour)
-    (.fillArc graphics inset inset (- width inset inset) (- height inset inset) 0 360)
-    (.setColor graphics Color/BLACK)
-    (.drawArc graphics inset inset (- width inset inset) (- height inset inset) 0 360)
+    (.fillArc graphics inset inset (- size inset inset) (- size inset inset) 0 360)
+    (.drawImage graphics shading 0 0 nil)
     image))
 
 (def ^:private empty-image
