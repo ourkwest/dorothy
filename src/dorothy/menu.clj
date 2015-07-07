27f0ddbb8792c6f75011ae8c61531f2eba416f5f
Peter Westmacott <peter.westmacott@gmail.com>
2015-07-07 21:47:03 +0100

    Add buttons to menu.

--- src/dorothy/menu.clj
+++ src/dorothy/menu.clj
@@ -1,8 +1,16 @@
 (ns dorothy.menu
-  (:import (java.awt MenuItem)))
+  (:import (java.awt MenuItem)
+           (java.awt.event ActionListener)))
 
 
 (defn divider [] (MenuItem. "-"))
 
 (defn label [text] (MenuItem. text))
+
+(defn button [text callback]
+  (doto (label text)
+    (.addActionListener
+      (reify ActionListener
+        (actionPerformed [this action]
+          (callback))))))
 
