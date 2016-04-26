64fb0475714de7b033275a221af72c45039c4da0
Peter Westmacott <peter.westmacott@gmail.com>
2016-04-26 21:17:07 +0100

    Update project for signed release.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject cotton/dorothy "1.0.1"
+(defproject cotton/dorothy "1.0.2"
   :description "A library to add clickable dots to the system tray."
   :url "https://github.com/peterwestmacott/dorothy"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]]
-  :deploy-repositories [["clojars" {:sign-releases false}]])
+  :signing {:gpg-key "peter.westmacott@gmail.com"})
